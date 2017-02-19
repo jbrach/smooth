b3a9f3dd6f5fc275241788b402139c72a1200b58
BigErn <brachjoe@gmail.com>
2017-02-19 15:05:38 -0500

    Created multiple projects including xunit test project is visual code

---
+++ test/Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace Tests
+{
+    public class DirectoryHelperTests
+    {
+        [Fact]
+        public void Test1() 
+        {
+            Assert.True(true);
+           // var helper = new DirectoryHelper();
+             
+        }
+        
+    }
+}
